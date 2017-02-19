4be8ff2f86d6afa1944b8ad535d278b51c60b089
sagefranch <sage.franch@outlook.com>
2017-02-18 16:53:58 -0800

    Full depluralization demo, bot takes user input and returns singular version

---
+++ PluralizationDemoBot/PluralizationDemoBot/Dialogs/PluralizationDialog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace seesharpbot.Dialogs
+{
+    [Serializable]
+    public class PluralizationDialog : IDialog<object>
+    {
+        public async System.Threading.Tasks.Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = (await argument).Text;
+            var depluralizer = PluralizationService.CreateService(new CultureInfo("en-US"));
+            var singular = message;
+            if (depluralizer.IsPlural(message))
+            {
+                //convert message to singular form
+                singular = depluralizer.Singularize(message);
+            }
+
+            await context.PostAsync($"You said {message}, the singular form is {singular}");
+            context.Wait(MessageReceivedAsync);
+
+        }
+    }
+}
---
+++ PluralizationDemoBot/PluralizationDemoBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PluralizationDemoBot.WebApiApplication" Language="C#" %>
